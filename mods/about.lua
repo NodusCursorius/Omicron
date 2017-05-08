b73dea78da1cf1f6301803c847204786540b7017
Nodus Cursorius <nodus.cursorius@gmail.com>
2017-05-08 10:00:57 -0700

    Actually update the about module this time
    Actually update the about module this time. Coffee pls.

--- mods/about.lua
+++ mods/about.lua
@@ -1,2 +1,2 @@
 client, message = ...
-message.channel:sendMessage(string.format('**Omicron**:\t\t\t\t\t\t\t\t`0.9`\n**Omicron codebase**:\t\t\t Github Link Goes Here\n**LuaJIT base**:\t\t\t\t\t\t  <https://luvit.io/>\n**Discord bot framework**:\t <https://github.com/SinisterRectus/Discordia>'))
+message.channel:sendMessage(string.format('**Omicron**:\t\t\t\t\t\t\t\t`0.9`\n**Omicron codebase**:\t\t\t <https://github.com/NodusCursorius/Omicron/>\n**LuaJIT base**:\t\t\t\t\t\t  <https://luvit.io/>\n**Discord bot framework**:\t <https://github.com/SinisterRectus/Discordia>'))
